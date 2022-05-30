71f710ca7a97a842a2e7f7ffea8f1669c420e7d5
Scott-McKool <scott.lr.cool@gmail.com>
2022-05-30 14:08:37 -0400

    converted code to class for use in other scripts

--- webcamDetection.py
+++ webcamDetection.py
@@ -3,27 +3,6 @@
 import cv2
 import detectConfig
 
-# detection threshold
-THRESHOLD = 0.5
-
-# id of v4l2 device 
-CAMERA_ID = 0
-
-# capture camera
-capture = cv2.VideoCapture(CAMERA_ID)
-
-# adjust settings
-capture.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
-capture.set(cv2.CAP_PROP_BRIGHTNESS,70)
-
- 
-net = cv2.dnn_DetectionModel(detectConfig.NETWORK_WEIGHTS_PATH,detectConfig.NETWORK_CONFIG_PATH)
-net.setInputSize(320,320)
-net.setInputScale(1.0/ 127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
-
 # class to smooth out position of detection across frames to avoid shakey aim
 # basically just the moving average function
 class smoother():
@@ -37,30 +16,90 @@ class smoother():
         self.accumulator = (self.alpha * value) + ((1.0 - self.alpha) * self.accumulator)
         return self.accumulator
     
-xpos = 0
-ypos = 0
+# class for detecting people using computer vision on a given webcam
+class personDetector():
+    def __init__(self, netWeights, netLabels, netConfig, smootherAlpha : float = 0.3):
+        self.weights = netWeights
+        self.labels = netLabels
+        self.config = netConfig
+        self.capture = None
+        self.network = None
+        self.initalizeNetwork()
+        self.xSmoother = None
+        self.ySmoother = None
 
-SMOOTHER_ALPHA = 0.3
+    def initalizeNetwork(self):
+        self.network = cv2.dnn_DetectionModel(self.weights, self.config)
+        self.network.setInputSize(320,320)
+        self.network.setInputScale(1.0/ 127.5)
+        self.network.setInputMean((127.5, 127.5, 127.5))
+        self.network.setInputSwapRB(True)
 
-xSmoother = smoother(SMOOTHER_ALPHA)
-ySmoother = smoother(SMOOTHER_ALPHA)
+    def startCamera(self, cameraID : int = 0, resolution : tuple = (640, 480)):
+        # if a camera is already reserved
+        if self.capture:
+            # release it 
+            self.capture.release()
 
+        # start up the video capture object 
+        self.capture = cv2.VideoCapture(cameraID)
+        # set the desired resolution
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH,resolution[0])
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT,resolution[1])
+
+    def captureFrame(self):
+        # make sure a camera capture is available
+        if not self.capture:
+            print("No videoCapture source")
+            # return false for the sucess var, and None for the frame var
+            return False, None
+
+        return self.capture.read()
+
+    def getDetectionPos(self, detectThreshold : float = 0.5):
+        # capture image
+        _, frame = self.captureFrame()
+
+        # run detection
+        classIds, confs, bbox = self.network.detect(frame,confThreshold=detectThreshold)
+
+        print(classIds)
+
+        # if at least one detection
+        if len(classIds) > 0:
+            # loop through detections 
+            for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox):
+                # only detect people (coco class id of 1)
+                if classId != 1:
+                    continue
+                # coords for center of detection
+                # xpos = x coordinate + width // 2
+                xpos = box[0] + box[2]//2
+                # ypos = y coordinate + height // 2
+                ypos = box[1] + box[3]//2
+                return xpos, ypos
+
+    def getDetectionPosSmooth(self, ALPHA : float = 0.3, detectThreshold : float = 0.5):
+        # initalize smoothers
+        if not self.xSmoother:
+            self.xSmoother = smoother(ALPHA)
+
+        if not self.ySmoother:
+            self.ySmoother = smoother(ALPHA)
+
+        # get pos from 1 frame
+        x, y = self.getDetectionPos(detectThreshold)
+
+        # smooth over 1/ALPHA frames
+        self.xPos = self.xSmoother.smooth(x)
+        self.yPos = self.ySmoother.smooth(y)
+
+        # return smoothed position
+        return self.xPos, self.yPos
+
+
+detector = personDetector(detectConfig.NETWORK_WEIGHTS_PATH, detectConfig.COCO_CLASS_NAMES, detectConfig.NETWORK_CONFIG_PATH)
+
+detector.startCamera()
 while True:
-    # capture an image
-    _, img = capture.read()
-
-    # run detection on the image
-    classIds, confs, bbox = net.detect(img,confThreshold=THRESHOLD)
-
-    # if at least one detection
-    if len(classIds) > 0:
-        # loop through detections 
-        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-            # only detect people (coco class id of 1)
-            if classId != 1:
-                continue
-            xpos = round( xSmoother.smooth(box[0] + box[2]//2))
-            ypos = round( ySmoother.smooth(box[1] + box[3]//2))
-            print(f"Person at {xpos}, {ypos}")
-
-capture.release()
+    detector.getDetectionPosSmooth()
