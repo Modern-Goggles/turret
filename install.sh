84ba5682b8b90001ef2bc0c9e901ac930974eca3
Scott-McKool <scott.lr.cool@gmail.com>
2022-06-03 22:33:25 -0400

    updated README to add instalation instructions

---
+++ install.sh
@@ -0,0 +1,26 @@
+# ths script installs the turret repo with the required dependencies and files
+
+# install dependencies
+sudo apt update 
+sudo apt install mpv -y
+
+# download network data
+wget https://github.com/Scott-McKool/turret/releases/download/downloads/ssdMobilenetV3.zip
+# make folder to put contents in
+mkdir ssdMobilenetV3
+# extract to folder
+unzip ssdMobilenetV3.zip -d ssdMobilenetV3/
+# clean up and remove the zip archive
+rm ssdMobilenetV3.zip
+
+# download sounds
+wget https://github.com/Scott-McKool/turret/releases/download/downloads/sounds.zip
+# make folder to put contents in
+mkdir sounds
+# extract to folder
+unzip sounds.zip -d sounds/
+# clean up and remove the zip archive
+rm sounds.zip
+
+# make the main file executable
+chmod +x turret.py
