a2c2a4f99f452ba8d8c27d4be6b8c66f47a34101
Scott-McKool <scott.lr.cool@gmail.com>
2022-06-18 14:33:31 -0400

    :(

--- sound.py
+++ sound.py
@@ -44,7 +44,7 @@ class soundPlayer():
         # choose a sound
         soundFile = choice(listdir(f"{self.soundDir}/target_spotted"))
         # starrt playing it
-        if soundPlayer.playSoundFile(f"{self.soundDir}/target_spotted"):
+        if soundPlayer.playSoundFile(f"{self.soundDir}/target_spotted/{soundFile}"):
             # if the soundplay returned true
             # mark the time 
             self.lastSound = time()
@@ -63,7 +63,7 @@ class soundPlayer():
         # choose a sound
         soundFile = choice(listdir(f"{self.soundDir}/target_lost"))
         # starrt playing it
-        if soundPlayer.playSoundFile(f"{self.soundDir}/target_lost"):
+        if soundPlayer.playSoundFile(f"{self.soundDir}/target_lost/{soundFile}"):
             # if the soundplay returned true
             # mark the time 
             self.lastSound = time()
