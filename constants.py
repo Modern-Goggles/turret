073150f223e15c0b78b3904d1112bd4e0e7179cf
Scott-McKool <scott.lr.cool@gmail.com>
2022-06-18 13:25:37 -0400

    convert sounds to class based system

--- constants.py
+++ constants.py
@@ -1,17 +1,27 @@
 ### basic configuration
+##############################
+
 # the absolute path to this repository
 # this gets populated automatically by install.sh
 REPO_DIR = ''
 
-### configuration for detection
+### configuratation for sounds
+##############################
 
-# path to config file
+# what directory are sounds in
+SOUNDS_DIR = f"{REPO_DIR}/sounds"
+
+
+### configuration for detection
+###############################
+
+# path to network config file
 NETWORK_CONFIG_PATH = f"{REPO_DIR}/ssdMobilenetV3/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
 
 # path to weights file
 NETWORK_WEIGHTS_PATH = f"{REPO_DIR}/ssdMobilenetV3/frozen_inference_graph.pb"
 
-# path to labls file
+# path to labels file
 LABELS_FILE = f"{REPO_DIR}/ssdMobilenetV3/coco.names"
 
 # extract coco class names
--- sound.py
+++ sound.py
@@ -1,16 +1,73 @@
+from random import choice
 from subprocess import Popen, DEVNULL
-from os import path
+from os import listdir, path
+from time import time
+import constants
 
-def playSoundFile(soundFile):
-    '''Spawn a new process that plays a sound file with mpv'''
-    # get path to given sound file
-    soundFile = path.abspath(soundFile)
-    # ensure it exists
-    if not path.exists(soundFile):
-        print(f"Cannot play sound, no such file {soundFile}")
-        return
-    # because this makes a new process and get piped to dev null, print that this sound is playing for logging purposes
-    print(f"playing sound {soundFile}")
-    # spawn a new process to use mpv to play the sound file
-    # use nohup to prevent the process from ending on hang up
-    Popen(["nohup", "mpv", "--no-video", f"{soundFile}"], stdout=DEVNULL, stderr=DEVNULL)
+class soundPlayer():
+    def __init__(self, soundDir):
+        # time to wait after playing a sound before playing another
+        self.soundCooldown = 3
+
+        # the last time a sound was played
+        self.lastSound = 0
+
+        # the directory for sounds
+        self.soundDir = soundDir
+
+    def playSoundFile(soundFile):
+        '''Spawn a new process that plays a sound file with mpv'''
+        # get path to given sound file
+        soundFile = path.abspath(soundFile)
+        # ensure it exists
+        if not path.exists(soundFile):
+            print(f"Cannot play sound, no such file {soundFile}")
+            return False
+        # because this makes a new process and get piped to dev null, print that this sound is playing for logging purposes
+        print(f"playing sound {soundFile}")
+        # spawn a new process to use mpv to play the sound file
+        # use nohup to prevent the process from ending on hang up
+        Popen(["nohup", "mpv", "--no-video", f"{soundFile}"], stdout=DEVNULL, stderr=DEVNULL)
+        return True
+
+    def playSound(self, soundFileName):
+        self.playSound(f"{self.soundDir}/{soundFileName}")
+
+    # TODO refactor these methods to reduce the ammount of repeated code
+    def targetSpotted(self):
+        '''play a sound signifying that a target has been spotted'''
+        # if the cooldawn has yet to pass
+        if time() - self.lastSound < self.soundCooldown:
+            # don't play a sound
+            return False
+        # if the cooldown has passed
+        # choose a sound
+        soundFile = choice(listdir(f"{self.soundDir}/target_spotted"))
+        # starrt playing it
+        if self.playSoundFile(soundFile):
+            # if the soundplay returned true
+            # mark the time 
+            self.lastSound = time()
+            # pass through the true return to signify sucess
+            return True
+        # return false to signify failure
+        return False
+
+    def targetLost(self):
+        '''play a sound signifying that a target has been spotted'''
+        # if the cooldawn has yet to pass
+        if time() - self.lastSound < self.soundCooldown:
+            # don't play a sound
+            return False
+        # if the cooldown has passed
+        # choose a sound
+        soundFile = choice(listdir(f"{self.soundDir}/target_lost"))
+        # starrt playing it
+        if self.playSoundFile(soundFile):
+            # if the soundplay returned true
+            # mark the time 
+            self.lastSound = time()
+            # pass through the true return to signify sucess
+            return True
+        # return false to signify failure
+        return False
